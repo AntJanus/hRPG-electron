83d67abe44f4742c1ce0192e52a46587ce48804b
antjanus <antonin@antjanus.com>
2016-04-23 00:43:48 -0600

    Electron setup updates

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 'use strict';
 
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const { app, BrowserWindow } = require('electron');
 
 const hRPGReduxLocation = '/hRPG-lite-redux/build/index.html';
 
@@ -17,9 +15,16 @@ app.on('window-all-closed', () => {
 
 app.on('ready', function() {
 
-  mainWindow = new BrowserWindow({ width: 925, height: 800 });
+  mainWindow = new BrowserWindow({
+    width: 925,
+    height: 800,
+    autoHideMenuBar: true,
+    titleBarStyle: 'hidden',
+    icon: __dirname + '/assets/images/hrpg-logo.png'
+  });
 
   mainWindow.loadURL('file://' + __dirname + hRPGReduxLocation);
+
 
   mainWindow.on('closed', () => {
     mainWindow = null;
